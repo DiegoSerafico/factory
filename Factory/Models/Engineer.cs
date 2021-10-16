33d13ca9e60118e4dac96f63a28a2ddb7501138c
Diego Serafico <dseraficohernandez@gmail.com>
2021-10-15 19:01:52 -0700

    fix error on engineer model

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -13,11 +13,5 @@ namespace Factory.Models
     public string Name { get; set; }
     public int Age { get; set; }
     public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
-
-    // private List<Engineer> GetEngineers()
-    // {
-    //   List<Engineer> engineers = new List<Engineer>();
-    //   return engineers;
-    // }
   }
 }
